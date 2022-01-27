27ca0ce3c2c40955284e47b5dd96c93d718c235d
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-01-27 20:32:36 +0900

    💄ProductDetail page markup + style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import logo from "../images/logo.png";
 function Header() {
   return (
     <header className="header">
       <h1>
-        <a href="#">
-          <img src="./images/logo.png" alt="오픈마켓" />
+        <a href="/">
+          <img src={logo} alt="오픈마켓" />
         </a>
       </h1>
       <nav>
