4f212c5b17c6aaa3eb8241a1ec1289116455308b
Jeong-ki <7863z@naver.com>
2022-02-01 18:16:17 +0900

    dashBoard style

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import DashHeader from '../components/DashHeader';
+import React from "react";
+import DashHeader from "../components/DashHeader";
+import test from "../images/icon-user.svg";
 
 function dashBoard() {
   return (
     <>
       <DashHeader />
       <section className="dashBoard">
-        <h2>대시보드 <strong>백엔드글로벌</strong></h2>
-        <button><span></span>상품업로드</button>
-        <ul>
-          <li>판매중인 상품(3)</li>
-          <li>주문/배송</li>
-          <li>문의/리뷰</li>
-          <li>통계</li>
-          <li>스토어 설정</li>
-        </ul>
-        <article>
-          <h3 className=".blind">업로드 상품목록</h3>
-          <div>
-            <p>상품정보</p>
-            <p>판매가격</p>
-            <p>수정</p>
-            <p>삭제</p>
-          </div>
-          <ul>
+        <h2>
+          대시보드<strong>백엔드글로벌</strong>
+        </h2>
+        <button className="uploadBtn">상품 업로드</button>
+        <div className="wrap">
+          <ul className="menu">
+            <li>판매중인 상품(3)</li>
             <li>
-              <img src="" alt="상품이미지" />
-              <div>
-                <p>딥러닝 개발자 무릎 담요</p>
-                <p>재고 : 370개</p>
-              </div>
-              <p>17,500원</p>
-              <button>수정</button>
-              <button>삭제</button>
+              주문/배송 <span>2</span>
             </li>
+            <li>
+              문의/리뷰<span>1</span>
+            </li>
+            <li>통계</li>
+            <li>스토어 설정</li>
           </ul>
-        </article>
+          <article>
+            <h3 className="blind">업로드 상품목록</h3>
+            <ul className="productInfo">
+              <li>상품정보</li>
+              <li>판매가격</li>
+              <li>수정</li>
+              <li>삭제</li>
+            </ul>
+            <ul className="products">
+              <li>
+                <div>
+                  <img src={test} alt="상품이미지" />
+                  <p>딥러닝 개발자 무릎 담요</p>
+                  <p>재고 : 370개</p>
+                </div>
+                <div className="price">17,500원</div>
+                <div className="box-update">
+                  <button className="update">수정</button>
+                </div>
+                <div className="box-delete">
+                  <button className="delete">삭제</button>
+                </div>
+              </li>
+            </ul>
+          </article>
+        </div>
       </section>
     </>
   );
