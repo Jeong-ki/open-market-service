921b19da08514e901458e2205ce6921b3eddc67d
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-02-01 14:53:42 +0900

    💄cart page add

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
-
+import test from "../images/test.jpg";
+import minus from "../images/icon-minus-line.svg";
+import plus from "../images/icon-plus-line.svg";
+import remove from "../images/icon-delete.svg";
 function CartPage() {
   return (
     <>
@@ -20,9 +23,68 @@ function CartPage() {
               <p>장바구니 안에 담긴 상품이 없습니다.</p>
               <p>원하는 상품을 장바구니에 담아보세요</p>
             </div>
-            <ul>
-              <li></li>
-            </ul>
+            <div>
+              <form action="#" method="get">
+                <ul>
+                  <li>
+                    <input type="checkbox" />
+                    <img src={test} alt="상품이미지" />
+                    <div>
+                      <p className="company">백엔드글로벌</p>
+                      <dl>
+                        <dt>딥러닝 개발자 무릎 담요</dt>
+                        <dd>
+                          <strong>17,500</strong>원
+                        </dd>
+                      </dl>
+                      <p className="delivery">택배배송 / 무료배송</p>
+                    </div>
+
+                    <div>
+                      <button type="button" className="symbol">
+                        <img src={minus} alt="minus" />
+                      </button>
+                      <span className="volume">1</span>
+                      <button type="button" className="symbol">
+                        <img src={plus} alt="plus" />
+                      </button>
+                    </div>
+                    <p>17,500원</p>
+                    <button type="button">주문하기</button>
+                    <button>
+                      <img src={remove} alt="상품 취소" />
+                    </button>
+                  </li>
+                </ul>
+                <div>
+                  <dl>
+                    <dt>총상품금액</dt>
+                    <dd>
+                      <strong>46,500</strong>원
+                    </dd>
+                  </dl>
+                  <dl>
+                    <dt>상품 할인</dt>
+                    <dd>
+                      <strong>0</strong>원
+                    </dd>
+                  </dl>
+                  <dl>
+                    <dt>배송비</dt>
+                    <dd>
+                      <strong>0</strong>원
+                    </dd>
+                  </dl>
+                  <dl>
+                    <dt>결제 예정 금액</dt>
+                    <dd>
+                      <strong>46,500</strong>원
+                    </dd>
+                  </dl>
+                </div>
+                <button type="submit">주문하기</button>
+              </form>
+            </div>
           </article>
         </div>
       </section>
