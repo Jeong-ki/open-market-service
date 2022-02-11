110a99bdb60eb325fbc78643b28fd5e15048bc87
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-02-11 19:35:02 +0900

    💄login page 모두  구현

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../images/logo.png";
-function login() {
+import axios from "axios";
+
+function Login() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginType, setLoginType] = useState("BUYER");
+  const [warningText, setWarningText] = useState("");
+
+  //포커스 이동하기 Ref
+  const idDom = React.useRef(null);
+  const passwordDom = React.useRef(null);
+
+  //이전 페이지로 이동을 위해 사용
+  let navigate = useNavigate();
+
+  //로그인 버튼 클릭시
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //아이디 또는 비밀번호 미기입 시
+    if (!id || !password) {
+      setWarningText("아이디 또는 비밀번호가 일치하지 않습니다.");
+      if (!id) {
+        idDom.current.focus();
+      } else if (!password) {
+        passwordDom.current.focus();
+      }
+      //아이디와 비밀번호 모두 기입 시
+    } else if (id && password) {
+      const data = {
+        username: id,
+        password: password,
+        login_type: loginType,
+      };
+      postData(data);
+    }
+  };
+
+  function postData(data) {
+    axios
+      .post("http://13.209.150.154:8000/accounts/login/", data)
+      .then((response) => {
+        localStorage.setItem("loginIdKey", response.data.token);
+        navigate(-1);
+      })
+      .catch(() => {
+        setPassword("");
+        setWarningText("아이디 또는 비밀번호가 일치하지 않습니다.");
+        passwordDom.current.focus();
+      });
+  }
+
   return (
     <div className="login">
       <h1>
@@ -8,34 +59,72 @@ function login() {
       </h1>
       <section>
         <h2 className="blind">로그인</h2>
-        <button className="loginChange purchase on">구매회원 로그인</button>
-        <button className="loginChange sale">판매회원 로그인</button>
-        <div class="loginForm">
+        <button
+          className={
+            "loginChange " + "purchase " + (loginType === "BUYER" ? "on" : "")
+          }
+          onClick={() => {
+            setLoginType("BUYER");
+          }}
+        >
+          구매회원 로그인
+        </button>
+        <button
+          className={
+            "loginChange " + "sale " + (loginType === "SELLER" ? "on" : "")
+          }
+          onClick={() => {
+            setLoginType("SELLER");
+          }}
+        >
+          판매회원 로그인
+        </button>
+        <div className="loginForm">
           <form action="/" method="post">
             <fieldset>
               <legend>구매회원 로그인</legend>
               <label>
-                <input type="text" placeholder="아이디" />
+                <input
+                  type="text"
+                  placeholder="아이디"
+                  autocomplete="off"
+                  onChange={(e) => {
+                    setId(e.target.value);
+                  }}
+                  value={id}
+                  ref={idDom}
+                />
               </label>
               <label>
-                <input type="password" placeholder="비밀번호" />
+                <input
+                  type="password"
+                  placeholder="비밀번호"
+                  autocomplete="off"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  value={password}
+                  ref={passwordDom}
+                />
               </label>
-              <p class="hide">아이디와 비밀번호가 일치하지 않습니다.</p>
-              <button type="submit">로그인</button>
+              <p className={warningText ? "" : "hide"}>{warningText}</p>
+              <button type="submit" onClick={handleSubmit}>
+                로그인
+              </button>
             </fieldset>
           </form>
         </div>
       </section>
       <div className="join">
         <span>
-          <a href="/">회원가입</a>
+          <a href="/register">회원가입</a>
         </span>
         <span>
-          <a href="/">비밀번호 찾기</a>
+          <a href="/login">비밀번호 찾기</a>
         </span>
       </div>
     </div>
   );
 }
 
-export default login;
+export default Login;
