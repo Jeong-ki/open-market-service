cdf5fd506e9669266bc146149bc2bc167d3745a1
Jeong-ki <7863z@naver.com>
2022-02-16 23:54:55 +0900

    ✨ 상품 삭제

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import DashHeader from "../components/DashHeader";
-import test from "../images/icon-user.svg";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react/cjs/react.development";
@@ -8,31 +7,84 @@ import { useState } from "react/cjs/react.development";
 function DashBoard() {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [delCheck, setDelCheck] = useState(true);
 
   useEffect(() => {
-    (async function postData() {
-      try {
-        const response = await axios.get(`http://13.209.150.154:8000/seller/`, {
-          headers: {
-            'Content-Type': 'application/json',
-             Authorization: `JWT ${localStorage.getItem("loginIdKey")}`,
-          }
-        });
-        if(response) {
-          console.log("response: ", response.data.results);
-          setProducts(response.data.results);
-        } else {
-          navigate("/error");
+    postData();
+  },[delCheck]);
+
+  async function postData() {
+    try {
+      const response = await axios.get(`http://13.209.150.154:8000/seller/`, {
+        headers: {
+          'Content-Type': 'application/json',
+           Authorization: `JWT ${localStorage.getItem("loginIdKey")}`,
         }
-        
-      } catch(error) {
-        // 응답 실패
-        alert("로그인 실패(중복된 암호 또는 전화번호)");
-        console.error("응답 실패", error);
+      });
+      if(response) {
+        console.log("response: ", response.data.results);
+        setProducts(response.data.results);
+      } else {
         navigate("/error");
       }
-    })();
-  }, []);
+      
+    } catch(error) {
+      // 응답 실패
+      alert("로그인 실패(중복된 암호 또는 전화번호)");
+      console.error("응답 실패", error);
+      navigate("/error");
+    }
+  };
+
+  async function productDel(product_id) {
+    console.log("삭제", product_id);
+    try {
+      const response = await axios.delete(`http://13.209.150.154:8000/products/${product_id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+           Authorization: `JWT ${localStorage.getItem("loginIdKey")}`,
+        }
+      });
+      if(response) {
+        console.log("del response: ", response);
+        setDelCheck(!delCheck);
+      } else {
+        navigate("/error");
+      }
+      
+    } catch(error) {
+      // 응답 실패
+      console.error("응답 실패", error);
+      navigate("/error");
+    }
+  };
+
+  function ProductList(props) {
+    return (
+      <ul className="products">
+        {
+          props.products.map((product) => {
+            return (
+              <li key={product.product_id}>
+                <div>
+                  <img src={product.image} alt={product.product_name + "이미지"} />
+                  <p>{product.product_name}</p>
+                  <p>재고 : {product.stock}개</p>
+                </div>
+                <div className="price">{product.price}원</div>
+                <div className="box-update">
+                  <button className="update">수정</button>
+                </div>
+                <div className="box-delete">
+                  <button onClick={() => { productDel(product.product_id) }} className="delete">삭제</button>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
 
   return (
     <>
@@ -42,7 +94,6 @@ function DashBoard() {
           대시보드<strong>백엔드글로벌</strong>
         </h2>
         <Link to="/addProduct" className="uploadBtn">상품 업로드</Link>
-        {/* <button className="uploadBtn">상품 업로드</button> */}
         <div className="wrap">
           <ul className="menu">
             <li>판매중인 상품(3)</li>
@@ -64,70 +115,11 @@ function DashBoard() {
               <li>삭제</li>
             </ul>
             <ProductList products={products} />
-            {/* <ul className="products">
-              
-              <li>
-                <div>
-                  <img src={test} alt="상품이미지" />
-                  <p>딥러닝 개발자 무릎 담요</p>
-                  <p>재고 : 370개</p>
-                </div>
-                <div className="price">17,500원</div>
-                <div className="box-update">
-                  <button className="update">수정</button>
-                </div>
-                <div className="box-delete">
-                  <button className="delete">삭제</button>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <img src={test} alt="상품이미지" />
-                  <p>딥러닝 개발자 무릎 담요</p>
-                  <p>재고 : 370개</p>
-                </div>
-                <div className="price">17,500원</div>
-                <div className="box-update">
-                  <button className="update">수정</button>
-                </div>
-                <div className="box-delete">
-                  <button className="delete">삭제</button>
-                </div>
-              </li>
-            </ul> */}
           </article>
         </div>
       </section>
     </>
   );
-}
-
-function ProductList(props) {
-  console.log(props.products);
-  return (
-    <ul className="products">
-      {
-        props.products.map((product) => {
-          return (
-            <li key={product.product_id}>
-              <div>
-                <img src={product.image} alt={product.product_name + "이미지"} />
-                <p>{product.product_name}</p>
-                <p>재고 : {product.stock}개</p>
-              </div>
-              <div className="price">{product.price}원</div>
-              <div className="box-update">
-                <button className="update">수정</button>
-              </div>
-              <div className="box-delete">
-                <button className="delete">삭제</button>
-              </div>
-            </li>
-          )
-        })
-      }
-    </ul>
-  )
 }
 
 export default DashBoard;
