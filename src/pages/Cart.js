ef21bb2827a898a6ae05be4bfa5f4ff43c6020cc
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-02-17 23:08:52 +0900

    💄cart page 기능 구현

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "../components/Header";
-import test from "../images/test.jpg";
 import minus from "../images/icon-minus-line.svg";
 import plus from "../images/icon-plus-line.svg";
 import remove from "../images/icon-delete.svg";
 function CartPage() {
+  let cartList;
+  // let [cartList, setCartList] = useState([]);
+  let products;
+  let [common, setCommon] = useState([]);
+  let [cartItemQuantity, setCartItemQuantity] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://13.209.150.154:8000/cart/", {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `JWT ${localStorage.getItem("acessToken")}`,
+        },
+      })
+      .then((response) => {
+        cartList = [...response.data.results];
+        let copyArr = [];
+        cartList.forEach((cartItem) => {
+          copyArr.push(cartItem.quantity);
+        });
+        setCartItemQuantity(copyArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://13.209.150.154:8000/products/")
+      .then((response) => {
+        products = [...response.data.results];
+        let newArr = [];
+        cartList.forEach((cartItem) => {
+          products.forEach((product) => {
+            if (cartItem.product_id === product.product_id) {
+              newArr.push(product);
+            }
+          });
+        });
+        setCommon(newArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function productCountPlusMinus(calculate, i) {
+    let copyArr = [...cartItemQuantity];
+    if (calculate === "plus") {
+      if (common[i].stock > cartItemQuantity[i]) {
+        ++copyArr[i];
+        setCartItemQuantity(copyArr);
+      }
+    } else if (calculate === "minus") {
+      if (cartItemQuantity[i] >= 2) {
+        --copyArr[i];
+        setCartItemQuantity(copyArr);
+      }
+    }
+  }
+
+  function cartItemDelete(cartId) {
+    console.log(cartId);
+  }
+
   return (
     <>
       <Header />
@@ -28,135 +94,16 @@ function CartPage() {
               </p>
               <p>원하는 상품을 장바구니에 담아보세요</p>
             </div>
+
             <div className="showProduct">
               <form action="#" method="get">
-                <ul>
-                  <li>
-                    <label>
-                      <p className="blind">상품 체크 박스</p>
-                      <input type="checkbox" className="hide" />
-                      <div>
-                        <span></span>
-                      </div>
-                    </label>
-
-                    <div className="introduce">
-                      <img src={test} alt="상품이미지" />
-                      <div className="introduceText">
-                        <p className="company">백엔드글로벌</p>
-                        <dl>
-                          <dt>딥러닝 개발자 무릎 담요</dt>
-                          <dd>
-                            <strong>17,500</strong>원
-                          </dd>
-                        </dl>
-                        <p className="delivery">택배배송 / 무료배송</p>
-                      </div>
-                    </div>
-
-                    <div className="quantity">
-                      <h3 className="blind">상품 수량 설정</h3>
-                      <div>
-                        <button type="button" className="symbol">
-                          <img src={minus} alt="minus" />
-                        </button>
-                        <span className="volume">1</span>
-                        <button type="button" className="symbol">
-                          <img src={plus} alt="plus" />
-                        </button>
-                      </div>
-                    </div>
-
-                    <div class="orderPrice">
-                      <p>17,500원</p>
-                      <button type="button" className="orderBtn">
-                        주문하기
-                      </button>
-                    </div>
-                    <button type="button" className="cancel">
-                      <img src={remove} alt="상품 취소" />
-                    </button>
-                  </li>
-                  <li>
-                    <label>
-                      <p className="blind">상품 체크 박스</p>
-                      <input type="checkbox" className="hide" />
-                      <div>
-                        <span></span>
-                      </div>
-                    </label>
-
-                    <div className="introduce">
-                      <img src={test} alt="상품이미지" />
-                      <div className="introduceText">
-                        <p className="company">백엔드글로벌</p>
-                        <dl>
-                          <dt>딥러닝 개발자 무릎 담요</dt>
-                          <dd>
-                            <strong>17,500</strong>원
-                          </dd>
-                        </dl>
-                        <p className="delivery">택배배송 / 무료배송</p>
-                      </div>
-                    </div>
-
-                    <div className="quantity">
-                      <h3 className="blind">상품 수량 설정</h3>
-                      <div>
-                        <button type="button" className="symbol">
-                          <img src={minus} alt="minus" />
-                        </button>
-                        <span className="volume">1</span>
-                        <button type="button" className="symbol">
-                          <img src={plus} alt="plus" />
-                        </button>
-                      </div>
-                    </div>
-
-                    <div class="orderPrice">
-                      <p>17,500원</p>
-                      <button type="button" className="orderBtn">
-                        주문하기
-                      </button>
-                    </div>
-                    <button type="button" className="cancel">
-                      <img src={remove} alt="상품 취소" />
-                    </button>
-                  </li>
-                </ul>
-
-                <div className="confirm">
-                  <dl className="total">
-                    <dt>총상품금액</dt>
-                    <dd>
-                      <strong>46,500</strong>원
-                    </dd>
-                  </dl>
-                  <div className="calculate minus">
-                    <span className="blind">-</span>
-                  </div>
-                  <dl className="cale">
-                    <dt>상품 할인</dt>
-                    <dd>
-                      <strong>0</strong>원
-                    </dd>
-                  </dl>
-                  <div className="calculate plus">
-                    <span className="blind">+</span>
-                  </div>
-                  <dl className="deliveryFee">
-                    <dt>배송비</dt>
-                    <dd>
-                      <strong>0</strong>원
-                    </dd>
-                  </dl>
-                  <dl className="payment">
-                    <dt>결제 예정 금액</dt>
-                    <dd>
-                      <strong>46,500</strong>원
-                    </dd>
-                  </dl>
-                </div>
+                <CartItem
+                  common={common}
+                  cartItemQuantity={cartItemQuantity}
+                  productCountPlusMinus={productCountPlusMinus}
+                  cartItemDelete={cartItemDelete}
+                />
+                <Confirm common={common} cartItemQuantity={cartItemQuantity} />
                 <button type="submit" className="submit">
                   주문하기
                 </button>
@@ -169,4 +116,154 @@ function CartPage() {
   );
 }
 
+function CartItem(props) {
+  let common = props.common;
+  let cartItemQuantity = props.cartItemQuantity;
+  return (
+    <ul>
+      {common.map((item, i) => {
+        return (
+          <li key={i}>
+            <label>
+              <p className="blind">상품 체크 박스</p>
+              <input type="checkbox" className="hide" />
+              <div>
+                <span></span>
+              </div>
+            </label>
+
+            <div className="introduce">
+              <img src={item.image} alt={item.product_name} />
+              <div className="introduceText">
+                <p className="company">{item.seller_store}</p>
+                <dl>
+                  <dt>{item.product_name}</dt>
+                  <dd>
+                    <strong>
+                      {(item.price + "").replace(
+                        /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
+                        ","
+                      )}
+                    </strong>
+                    원
+                  </dd>
+                </dl>
+                <p className="delivery">
+                  {item.shipping_method === "DELIVERY"
+                    ? "택배배송 "
+                    : "소포배송 "}
+                  /
+                  {item.shipping_fee === 0
+                    ? " 무료배송"
+                    : " " + item.shipping_fee + "원"}
+                </p>
+              </div>
+            </div>
+
+            <div className="quantity">
+              <h3 className="blind">상품 수량 설정</h3>
+              <div>
+                <button
+                  type="button"
+                  className="symbol"
+                  onClick={() => {
+                    props.productCountPlusMinus("minus", i);
+                  }}
+                >
+                  <img src={minus} alt="minus" />
+                </button>
+                <span className="volume">{cartItemQuantity[i]}</span>
+                <button
+                  type="button"
+                  className="symbol"
+                  onClick={() => {
+                    props.productCountPlusMinus("plus", i);
+                  }}
+                >
+                  <img src={plus} alt="plus" />
+                </button>
+              </div>
+            </div>
+
+            <div className="orderPrice">
+              <p>
+                <strong>{item.price * cartItemQuantity[i]}</strong>원
+              </p>
+              <button type="button" className="orderBtn">
+                주문하기
+              </button>
+            </div>
+            <button
+              type="button"
+              className="cancel"
+              onClick={() => {
+                console.log(common);
+                props.cartItemDelete(item.cart_item_id);
+              }}
+            >
+              <img src={remove} alt="상품 취소" />
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Confirm(props) {
+  let common = props.common;
+  let cartItemQuantity = props.cartItemQuantity;
+  let totalAmount = 0;
+  let fee = 0;
+  common.forEach((item, i) => {
+    totalAmount += item.price * cartItemQuantity[i];
+    fee += item.shipping_fee;
+  });
+  return (
+    <div className="confirm">
+      <dl className="total">
+        <dt>총상품금액</dt>
+        <dd>
+          <strong>
+            {(totalAmount + "").replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+          </strong>
+          원
+        </dd>
+      </dl>
+      <div className="calculate minus">
+        <span className="blind">-</span>
+      </div>
+      <dl className="cale">
+        <dt>상품 할인</dt>
+        <dd>
+          <strong>0</strong>원
+        </dd>
+      </dl>
+      <div className="calculate plus">
+        <span className="blind">+</span>
+      </div>
+      <dl className="deliveryFee">
+        <dt>배송비</dt>
+        <dd>
+          <strong>
+            {(fee + "").replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+          </strong>
+          원
+        </dd>
+      </dl>
+      <dl className="paymentDl">
+        <dt>결제 예정 금액</dt>
+        <dd>
+          <strong>
+            {(fee + totalAmount + "").replace(
+              /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
+              ","
+            )}
+          </strong>
+          원
+        </dd>
+      </dl>
+    </div>
+  );
+}
 export default CartPage;
